8d8d959bec7907794ccadfbea8c6d8bd3492c72a
ekimov_vd <ekimov_vd@mail.ru>
2024-10-13 12:52:14 +0300

    Чистая Архитектура. Внедрение Зависимостей

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,16 @@ import (
 
 	"github.com/ekimovvd/todo-backend"
 	"github.com/ekimovvd/todo-backend/pkg/handler"
+	"github.com/ekimovvd/todo-backend/pkg/repository"
+	"github.com/ekimovvd/todo-backend/pkg/service"
 )
 
 func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	handlers := new(handler.Handler)
 
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
