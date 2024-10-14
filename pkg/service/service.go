04cdc15520819029719cc4d76742b740a3146a54
ekimov_vd <ekimov_vd@mail.ru>
2024-10-14 11:34:08 +0300

    Регистрация пользователей

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,8 +1,13 @@
 package service
 
-import "github.com/ekimovvd/todo-backend/pkg/repository"
+import (
+	"github.com/ekimovvd/todo-backend"
+	"github.com/ekimovvd/todo-backend/pkg/repository"
+)
 
-type Authorization interface{}
+type Authorization interface {
+	CreateUser(user todo.User) (int, error)
+}
 
 type TodoList interface{}
 
@@ -15,5 +20,7 @@ type Service struct {
 }
 
 func NewService(repos *repository.Repository) *Service {
-	return &Service{}
+	return &Service{
+		Authorization: NewAuthService(repos.Authorization),
+	}
 }
